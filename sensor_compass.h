43c33c6f6f51365facb42c748c8ffe16937c909f
Sergei V. Kravchenko <kravserg1032@gmail.com>
2026-01-02 19:38:18 +0100

    Disabled compass; It blocks booting process if it is not cennected

--- sensor_compass.h
+++ sensor_compass.h
@@ -11,18 +11,19 @@ public:
     }
 
     void init() {
-        Wire.begin();
-        compass.init();
-        compass.enableDefault();
+        // TODO: compass may block booting process
+        // Wire.begin();
+        // compass.init();
+        // compass.enableDefault();
 
         /*
         Calibration values; the default values of +/-32767 for each axis
         lead to an assumed magnetometer bias of 0. Use the Calibrate example
         program to determine appropriate values for your particular unit.
         */
-        compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
-        compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
-        
+        // compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+        // compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+
         ts_ = millis();
     }
 
@@ -33,8 +34,8 @@ public:
     void process() {
         uint32_t now = millis();
         if (now - ts_ >= 500) { // Update heading every 500 ms
-            compass.read();
-            heading_ = compass.heading();
+            // compass.read();
+            // heading_ = compass.heading();
             ts_ = now;
         }
     }
